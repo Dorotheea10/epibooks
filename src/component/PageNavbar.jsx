77b152eea3077b35e4929b837bfab40bccaac6f0
Dorotheea10 <150785849+Dorotheea10@users.noreply.github.com>
2024-03-12 17:12:52 +0100

    React Epibook

---
+++ src/component/PageNavbar.jsx
@@ -0,0 +1,25 @@
+// dobbiamo aggiungere gli IMPORT degli elementi di react bootstrap
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+
+const PageNavbar = function (props) {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" data-bs-theme="dark">
+      <Container fluid>
+        <Navbar.Brand href="#home">Epibooks- {props.subtitle}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#menu">Menu</Nav.Link>
+            <Nav.Link href="#booking">Prenotazioni</Nav.Link>
+            <Nav.Link href="#contacts">Contatti</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default PageNavbar;
